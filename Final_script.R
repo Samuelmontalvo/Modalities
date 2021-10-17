69595e2337665dceebe23c9f0c9c1c2b974d7dd9
Samuelmontalvo <smontalvo@miners.utep.edu>
2021-10-17 11:58:31 -0600

    modalities update

--- Final_script.R
+++ Final_script.R
@@ -1,7 +1,6 @@
 library(readxl)
-Final_Data <- read_excel("Final_Data.xlsx",
-                         sheet = "Couri")
-View(Final_Data)
+Final_Data <- read_excel("Data.xlsx", sheet = "Data")
+View(Data)
 attach(Final_Data)
 #priori power analysis
 library(pwr)
@@ -22,10 +21,21 @@ Final_Data <- Final_Data %>%
 #subset for baseline TABLES
 Baseline <- Final_Data %>% filter(Modality == "Baseline")
 
-drop <- c("ID","Modality","Intensity","Load","LOAD_norm","Squat","Bench","Biceps","RPE")
+drop <- c("ID","Modality","Intensity","Load","LOAD_norm","Squat",
+          "Bench","Biceps","RPE")
 
 Baseline <- Baseline[,!(names(Baseline) %in% drop)]
 
+
+Males <- Baseline %>% filter(Sex=="Male")
+Females <- Baseline %>% filter(Sex=="Females")
+
+t.test(lactate ~ Sex, alternative = "greater", var.equal = FALSE)
+
+cor.test(Squat_kg,VO2_Bike,method="spearman")
+
+model <- lm(Bench_kg~VO2_Treadmill, data = Baseline)
+summary(model)
 
 library(psych)
 #Descriptives for all
@@ -76,17 +86,19 @@ tbl_baseline_Female %>% kable()%>%
 
 #create data frame and drop unused columns
 SA <- Final_Data %>% select("ID","Sex","Intensity","Modality","VO2","Diameter",
-                            "Velocity","Velocity_Ret")
+                            "Velocity","Velocity_Ret", "ESS", "RE_B")
+
+SA <-SA[!(SA$Modality=="Baseline"),]
 
 ##Normality Test
 library(rstatix)
-SA %>% shapiro_test(Velocity_Ret)
+SA %>% shapiro_test(Diameter)
 attach(SA)
 #General Linear Mixed models
 
 library(lme4)
 library(lmerTest)
-lmModel = lmer(Velocity ~ Modality*Intensity + (1|ID), data=SA, REML=TRUE)
+lmModel = lmer(RE_B ~ Modality*Intensity + (1|ID), data=SA, REML=TRUE)
 # mixed model
 anova(lmModel)
 #test of the random effects in the model
@@ -95,13 +107,13 @@ rand(lmModel)
 attach(SA)
 pwc <- SA %>%
   group_by(Intensity) %>%
-  pairwise_t_test(Velocity ~ Modality, paired = T,
-                  p.adjust.method	= "none")
+  pairwise_t_test(RE_B ~ Modality, paired = T,
+                  p.adjust.method	= "holm")
 pwc
 
 # Effect size Cohen's D with Hedge's g correction for small sample size
 SA %>%
-  group_by(Intensity)  %>% cohens_d(Velocity ~ Modality,
+  group_by(Intensity)  %>% cohens_d(RE_B ~ Modality,
                                     paired = TRUE, hedges.correction = TRUE)
 
 
@@ -110,11 +122,11 @@ SA %>%
 library(ggplot2)
 library(ggpubr)
 # Add position for p values in boxplot
-pwc <- pwc %>% add_xy_position(x = "Intensity")
+pwc <- pwc %>% add_xy_position(x = "RE_B")
 # Boxplot of Vertical Jump Height
-ggboxplot(SA, x = "Intensity", y = "Velocity",
+ggboxplot(SA, x = "Intensity", y = "RE_B",
           color = "Modality", palette = get_palette("Dark2", 7),
-          ylab = "Anterograde Velocity (cm/s)") +
+          ylab = "RE_B ") +
   stat_pvalue_manual(pwc,size = 5,hide.ns = TRUE)
 
 ggplot(SA,aes(Intensity,Velocity, fill= factor(Modality))) +
