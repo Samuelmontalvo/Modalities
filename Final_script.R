424c66e8724f71ef3656d5f9ca63785ac168051b
Samuelmontalvo <smontalvo@miners.utep.edu>
2022-03-29 12:00:44 -0600

    a

--- Final_script.R
+++ Final_script.R
@@ -17,7 +17,8 @@ library(cvcqv)
 
 #priori power analysis
 library(pwr)
-pwr.anova.test(f=0.5,k=14,n=6,sig.level=0.05)
+anova_pwr <- pwr.anova.test(f=0.4,k=14,n=14,sig.level=0.05)
+plot(anova_pwr)
 
 ## convert sex and intensity to factors from characters
 Df <- Df %>%
@@ -170,13 +171,94 @@ effect %>% kable()%>%
 #Plots
 # Boxplot of ESS between
 
-pwc <- pwc %>% add_xy_position(x = "Intensity")
-ESS_between_plot <- ggboxplot(Df, x = "Intensity", y = "ESS",
+ESS_between_plot <-ggboxplot(Df, x = "Intensity", y = "ESS",
                           color = "Modality", palette = get_palette("Dark2", 8),
                           ylab = "Endothelial Shear Stress (dynes/cm2)") +
-  stat_pvalue_manual(pwc,size = 7,hide.ns = TRUE,"p.adj.signif")
+  ##first segment is the line, second is left bracket, third is right
+  ##Cycle vs bench low
+  geom_segment(aes(x = 1.8, y = 110, xend = 2.2, yend = 110))+
+  geom_segment(aes(x = 1.8, y = 108, xend = 1.8, yend = 110)) +
+  geom_segment(aes(x = 2.2, y = 108, xend = 2.2, yend = 110)) +
+  annotate(geom="text", x=2.0, y=111, label="*",color="Black") +
+  ## Cycle vs bench moderate
+  geom_segment(aes(x = 2.8, y = 110, xend = 3.2, yend = 110)) +
+  geom_segment(aes(x = 2.8, y = 108, xend = 2.8, yend = 110)) +
+  geom_segment(aes(x = 3.2, y = 108, xend = 3.2, yend = 110)) +
+  annotate(geom="text", x=3.0, y=111, label="*",color="Black") +
+  ## Cycle vs bench high
+  geom_segment(aes(x = 3.8, y = 110, xend = 4.2, yend = 110)) +
+  geom_segment(aes(x = 3.8, y = 108, xend = 3.8, yend = 110)) +
+  geom_segment(aes(x = 4.2, y = 108, xend = 4.2, yend = 110)) +
+  annotate(geom="text", x=4.0, y=111, label="* *",color="Black") +
+  ## Bench vs squat low
+  geom_segment(aes(x = 2.2, y = 115, xend = 2.33, yend = 115)) +
+  geom_segment(aes(x = 2.2, y = 113, xend = 2.2, yend = 115)) +
+  geom_segment(aes(x = 2.33, y = 113, xend = 2.33, yend = 115)) +
+  annotate(geom="text", x=2.27, y=116, label="*",color="Black") +
+  ## arm ergometer vs treadmill moderate
+  geom_segment(aes(x = 2.67, y = 120, xend = 2.93, yend = 120)) +
+  geom_segment(aes(x = 2.67, y = 118, xend = 2.67, yend = 120)) +
+  geom_segment(aes(x = 2.93, y = 118, xend = 2.93, yend = 120)) +
+  annotate(geom="text", x=2.8, y=121, label="*",color="Black") +
+  ## arm ergometer vs treadmill High
+  geom_segment(aes(x = 3.67, y = 120, xend = 3.93, yend = 120)) +
+  geom_segment(aes(x = 3.67, y = 118, xend = 3.67, yend = 120)) +
+  geom_segment(aes(x = 3.93, y = 118, xend = 3.93, yend = 120)) +
+  annotate(geom="text", x=3.8, y=121, label="* *",color="Black") +
+  ## Cycle ergometer vs biceps moderate
+  geom_segment(aes(x = 2.8, y = 125, xend = 3.07, yend = 125)) +
+  geom_segment(aes(x = 2.8, y = 123, xend = 2.8, yend = 125)) +
+  geom_segment(aes(x = 3.07, y = 123, xend = 3.07, yend = 125)) +
+  annotate(geom="text", x=2.94, y=126, label="*",color="Black") +
+  ## Cycle ergometer vs biceps high
+  geom_segment(aes(x = 3.8, y = 125, xend = 4.07, yend = 125)) +
+  geom_segment(aes(x = 3.8, y = 123, xend = 3.8, yend = 125)) +
+  geom_segment(aes(x = 4.07, y = 123, xend = 4.07, yend = 125)) +
+  annotate(geom="text", x=3.94, y=126, label="* *",color="Black") +
+  ## Treadmill vs biceps moderate
+  geom_segment(aes(x = 2.93, y = 130, xend = 3.07, yend = 130)) +
+  geom_segment(aes(x = 2.93, y = 128, xend = 2.93, yend = 130)) +
+  geom_segment(aes(x = 3.07, y = 128, xend = 3.07, yend = 130)) +
+  annotate(geom="text", x=3.0, y=131, label="* * *",color="Black")  +
+  ## Treadmill vs biceps high
+  geom_segment(aes(x = 3.93, y = 130, xend = 4.07, yend = 130)) +
+  geom_segment(aes(x = 3.93, y = 128, xend = 3.93, yend = 130)) +
+  geom_segment(aes(x = 4.07, y = 128, xend = 4.07, yend = 130)) +
+  annotate(geom="text", x=4.0, y=131, label="* * *",color="Black")  +
+  ## Treadmill vs bench moderate
+  geom_segment(aes(x = 2.93, y = 135, xend = 3.2, yend = 135)) +
+  geom_segment(aes(x = 2.93, y = 133, xend = 2.93, yend = 135)) +
+  geom_segment(aes(x = 3.2, y = 133, xend = 3.2, yend = 135)) +
+  annotate(geom="text", x=3.07, y=136, label="* * *",color="Black") +
+  ## Treadmill vs bench high
+  geom_segment(aes(x = 3.93, y = 135, xend = 4.2, yend = 135)) +
+  geom_segment(aes(x = 3.93, y = 133, xend = 3.93, yend = 135)) +
+  geom_segment(aes(x = 4.2, y = 133, xend = 4.2, yend = 135)) +
+  annotate(geom="text", x=4.07, y=136, label="* * *",color="Black")   +
+  ## Treadmill vs squat moderate
+  geom_segment(aes(x = 2.93, y = 140, xend = 3.33, yend = 140)) +
+  geom_segment(aes(x = 2.93, y = 138, xend = 2.93, yend = 140)) +
+  geom_segment(aes(x = 3.33, y = 138, xend = 3.33, yend = 140)) +
+  annotate(geom="text", x=3.13, y=141, label="*",color="Black")  +
+  ## Treadmill vs squat high
+  geom_segment(aes(x = 3.93, y = 140, xend = 4.33, yend = 140)) +
+  geom_segment(aes(x = 3.93, y = 138, xend = 3.93, yend = 140)) +
+  geom_segment(aes(x = 4.33, y = 138, xend = 4.33, yend = 140)) +
+  annotate(geom="text", x=4.13, y=141, label="* * *",color="Black") +
+  ## biceps vs squat moderate
+  geom_segment(aes(x = 3.07, y = 145, xend = 3.33, yend = 145)) +
+  geom_segment(aes(x = 3.07, y = 143, xend = 3.07, yend = 145)) +
+  geom_segment(aes(x = 3.33, y = 143, xend = 3.33, yend = 145)) +
+  annotate(geom="text", x=3.2, y=146, label="*",color="Black") +
+  ## cycle ergometer vs squat moderate
+  geom_segment(aes(x = 3.8, y = 150, xend = 4.33, yend = 150)) +
+  geom_segment(aes(x = 3.8, y = 148, xend = 3.8, yend = 150)) +
+  geom_segment(aes(x = 4.33, y = 148, xend = 4.33, yend = 150)) +
+  annotate(geom="text", x=4.065, y=151, label="*",color="Black")
+
 ESS_between_plot
 ggsave("ESS_between_plot.png")
+ggsave("ESS_between_plot.jpeg")
 
 # Boxplot of ESS within
 ESS_within_plot <- ggboxplot(Df, x = "Intensity", y = "ESS",
@@ -197,17 +279,18 @@ ESS_within_plot <- ggboxplot(Df, x = "Intensity", y = "ESS",
   geom_segment(aes(x = 2.07, y = 15, xend = 4.08, yend = 15)) +
   geom_segment(aes(x = 2.07, y = 17, xend = 2.07, yend = 15)) +
   geom_segment(aes(x = 4.079, y = 17, xend = 4.079, yend = 15)) +
-  annotate(geom="text", x=3.07, y=13.5, label="x",color="Black") +
+  annotate(geom="text", x=3.07, y=13.5, label="‡",color="Black") +
   geom_segment(aes(x = 2.2, y = 10, xend = 3.21, yend = 10)) +
   geom_segment(aes(x = 2.2, y = 12, xend = 2.2, yend = 10)) +
   geom_segment(aes(x = 3.207, y = 12, xend = 3.207, yend = 10)) +
-  annotate(geom="text", x=2.7, y=8.5, label="x",color="Black") +
+  annotate(geom="text", x=2.7, y=8.5, label="†",color="Black") +
   geom_segment(aes(x = 2.35, y = 5, xend = 4.35, yend = 5)) +
   geom_segment(aes(x = 2.35, y = 7, xend = 2.35, yend = 5)) +
   geom_segment(aes(x = 4.3457, y = 7, xend = 4.3457, yend = 5)) +
-  annotate(geom="text", x=3.3, y=3, label="x",color="Black")
+  annotate(geom="text", x=3.3, y=3, label="‡",color="Black")
 ESS_within_plot
 ggsave("ESS_within_plot.png")
+ggsave("ESS_within_plot.jpeg")
 
 
 ## RE
@@ -249,3 +332,4 @@ Reynolds_error_plot <- ggerrorplot(Df, x = "Intensity", y = "RE_B",
   geom_hline(yintercept=2000, linetype="dashed", color = "red")
 Reynolds_error_plot
 ggsave("Reynolds_error_plot.png")
+ggsave("Reynolds_error_plot.jpeg")
